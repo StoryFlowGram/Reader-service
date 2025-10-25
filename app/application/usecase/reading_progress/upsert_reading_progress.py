6e04862c97885e7109ba84dad5f6b4192ccf62f4
Squilix01 <Squilix01@no-reply.com>
2025-10-26 00:22:36 +0300

    fix(application/usecase): fix, refactor and migrate to UoW pattern

--- app/application/usecase/reading_progress/upsert_reading_progress.py
+++ app/application/usecase/reading_progress/upsert_reading_progress.py
@@ -1,43 +1,37 @@
-from app.domain.protocols.user_book_protocol import UserBookProtocol
-from app.domain.protocols.reading_progress_protocol import ReadingProgressProtocol
+from app.domain.exception.user_book import (
+    UserBookNotFound, ProgressError
+)
+
+from app.application.interfaces.uow import UnitOfWorkInterface
 from app.domain.entity.reading_progress import ReadingProgressDomain
-from app.domain.entity.user_book import UserBook
 from app.application.dto.reading_progress.reading_progress_dto import ReadingProgressDTO
 
-class UpsertReadingProgress:
-    def __init__(self, progress_repo: ReadingProgressProtocol, user_book_repo: UserBookProtocol) -> None:
-        self.progress_repo = progress_repo
-        self.user_book_repo = user_book_repo
+class UpsertReadingProgressUseCase:
+    def __init__(self, uow: UnitOfWorkInterface): 
+        self.uow = uow
 
-    async def __call__(self, reading_progress: ReadingProgressDomain) -> ReadingProgressDTO:
-
-        user_book: UserBook = await self.user_book_repo.get_by_user_book_id(reading_progress.user_book_id)
-        
-        if not user_book:
-            raise ValueError(f"Книга з user_book_id={reading_progress.user_book_id} не знайдена в бібліотеці.")
-
-        existing_progress = await self.progress_repo.get_by_user_book(reading_progress.user_book_id)
-        
-        saved_progress: ReadingProgressDomain
-
-        if existing_progress:
-            existing_progress.position = reading_progress.position
-            existing_progress.chapter_id = reading_progress.chapter_id
+    async def __call__(self, progress_entity: ReadingProgressDomain) -> ReadingProgressDTO:
+        async with self.uow:
+            user_book = await self.uow.user_books.get_by_user_book_id(progress_entity.user_book_id)
+            if not user_book:
+                raise UserBookNotFound("Книга не найдена ")
             
-            saved_progress = await self.progress_repo.update(existing_progress)
+            check_exist_progress = await self.uow.reading_progress.get_by_user_book(progress_entity.user_book_id)
+            if check_exist_progress:
+                check_exist_progress.position = progress_entity.position
+                check_exist_progress.chapter_id = progress_entity.chapter_id
             
-        else:
-            saved_progress = await self.progress_repo.add(reading_progress)
-
-        if not saved_progress:
-            raise Exception("Не вдалося зберегти прогрес читання. Репозиторій повернув None.")
+                saved_progress = await self.uow.reading_progress.update(check_exist_progress)
+            else:
+                saved_progress = await self.uow.reading_progress.add(progress_entity)
+            
+            if not saved_progress:
+                raise ProgressError("Не удалось сохранить объект")
         
-        await self.user_book_repo.update(
-            user_id=user_book.user_id,      
-            book_id=user_book.book_id,       
-            overall_progress=saved_progress.position  
-        )
+            user_book.overall_progress = saved_progress.position
 
+            await self.uow.user_books.update(user_book.user_id, user_book.book_id)
+        
         return ReadingProgressDTO(
             id=saved_progress.id,
             user_book_id=saved_progress.user_book_id,
