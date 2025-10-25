70c02d9ea91f8740fe9f4c95224a10d9a44cc8db
Squilix01 <Squilix01@no-reply.com>
2025-10-26 00:44:19 +0300

    feat(infra/di): add uow dependency and book-service provider(now a stub)

--- app/infrastructure/di.py
+++ app/infrastructure/di.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 
+from app.infrastructure.database.uow import UnitOfWork
 from app.infrastructure.repositories.user_book_repo import UserBookRepository
 from app.infrastructure.repositories.reading_progress_repo import ReadingProgressRepository
+from app.infrastructure.service.book_service import BookService
 from app.infrastructure.database.session import get_session
 
 
@@ -14,3 +16,17 @@ async def book_protocol(session:AsyncSession = Depends(get_session)):
 
 async def reading_progress_protocol(session:AsyncSession = Depends(get_session)):
     return ReadingProgressRepository(session)
+
+
+def get_book_service_provider() :
+    return BookService()
+
+
+async def uow_dependency():
+    session: AsyncSession = await get_session()
+    uow = UnitOfWork(session)
+    
+    try:
+        return uow
+    finally:
+        await session.close()
