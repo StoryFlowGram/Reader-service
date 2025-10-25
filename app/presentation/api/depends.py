c46d52161b64b8466179e716990984b0dd282f42
Squilix01 <Squilix01@no-reply.com>
2025-10-26 00:50:52 +0300

    feat(presentation): add controller for user book and depends.py

--- app/presentation/api/depends.py
+++ app/presentation/api/depends.py
@@ -1,11 +1,12 @@
-
-
-
-
 async def book_protocol():
     raise NotImplementedError("Должен быть переопределён в инфра слое")
 
 
-
 async def reading_progress_protocol():
+    raise NotImplementedError("Должен быть переопределён в инфра слое")
+
+async def uow_dependency():
+    raise NotImplementedError("Должен быть переопределён в инфра слое")
+
+async def book_service_provider():
     raise NotImplementedError("Должен быть переопределён в инфра слое")
--- app/presentation/api/v1/user_book/user_book_controller.py
+++ app/presentation/api/v1/user_book/user_book_controller.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
 
-from app.presentation.api.depends import book_protocol
-from app.application.usecase.user_book.add_user_book import AddBookToLibrary
-from app.application.usecase.user_book.get_user_book import GetUserBook
-from app.application.usecase.user_book.list_user_book import ListUserBooks
-from app.application.usecase.user_book.remove_user_book import RemoveBookFromLibrary
-from app.application.usecase.user_book.update_user_book import UpdateUserBook
+
+from app.presentation.api.depends import uow_dependency
+from app.domain.entity.user_book import UserBook
+from app.domain.exception.user_book import BookAlreadyInLibrary
+from app.application.interfaces.uow import UnitOfWorkInterface
+from app.application.usecase.user_book.add_user_book import AddBookToLibraryUseCase
+from app.application.usecase.user_book.get_user_book import GetUserBookUseCase
+from app.application.usecase.user_book.list_user_book import ListUserBooksUseCase
+from app.application.usecase.user_book.remove_user_book import RemoveBookFromLibraryUseCase
+from app.application.usecase.user_book.update_user_book import UpdateUserBookUseCase
 from app.presentation.schemas.user_book.user_book_schemas import AddRequestSchema, AddResponseSchema, UpdateRequestSchema
 
 
@@ -16,17 +20,26 @@ user_book_router = APIRouter(
 
 
 @user_book_router.post("/add", response_model=AddResponseSchema)
-async def add_user_book(add_schema: AddRequestSchema, protocol = Depends(book_protocol)):
-    usecase = AddBookToLibrary(protocol)
+async def add_user_book(
+    add_schema: AddRequestSchema, 
+    uow: UnitOfWorkInterface = Depends(uow_dependency)
+):
+    usecase = AddBookToLibraryUseCase(uow)
+    user_book_entity = UserBook(**add_schema.model_dump())
     try:
-        add_book = await usecase(add_schema)
+        add_book = await usecase(user_book_entity) 
         return add_book
+    except BookAlreadyInLibrary as e:
+        raise HTTPException(status_code=409, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
 @user_book_router.get("/get_book", response_model=AddResponseSchema)
-async def get_user_book(user_id: int,book_id: int,protocol = Depends(book_protocol)):
-    usecase = GetUserBook(protocol)
+async def get_user_book(
+    user_id: int,
+    book_id: int,
+    uow: UnitOfWorkInterface = Depends(uow_dependency)):
+    usecase = GetUserBookUseCase(uow)
     try:
         get_book = await usecase(user_id,book_id)
         return get_book
@@ -34,8 +47,10 @@ async def get_user_book(user_id: int,book_id: int,protocol = Depends(book_protoc
         raise HTTPException(status_code=400, detail=str(e))
     
 @user_book_router.get("/list", response_model=list[AddResponseSchema])
-async def list_user_book(user_id: int, protocol = Depends(book_protocol)):
-    usecase = ListUserBooks(protocol)
+async def list_user_book(
+    user_id: int, 
+    uow: UnitOfWorkInterface = Depends(uow_dependency)):
+    usecase = ListUserBooksUseCase(uow)
     try:
         get_list_book = await usecase(user_id)
         return get_list_book
@@ -44,8 +59,12 @@ async def list_user_book(user_id: int, protocol = Depends(book_protocol)):
     
 
 @user_book_router.put("/update", response_model=AddResponseSchema)
-async def update_user_book(user_id: int, book_id: int,  update_schema: UpdateRequestSchema, protocol = Depends(book_protocol)):
-    usecase = UpdateUserBook(protocol)
+async def update_user_book(
+    user_id: int, 
+    book_id: int,  
+    update_schema: UpdateRequestSchema, 
+    uow: UnitOfWorkInterface = Depends(uow_dependency)):
+    usecase = UpdateUserBookUseCase(uow)
     try:
         update_book = await usecase(user_id,book_id,update_schema.overall_progress)
         return update_book
@@ -53,8 +72,11 @@ async def update_user_book(user_id: int, book_id: int,  update_schema: UpdateReq
         raise HTTPException(status_code=400, detail=str(e))
     
 @user_book_router.delete("/delete")
-async def remove_user_book(user_id: int, book_id: int,protocol = Depends(book_protocol)):
-    usecase = RemoveBookFromLibrary(protocol)
+async def remove_user_book(
+    user_id: int, 
+    book_id: int,
+    uow: UnitOfWorkInterface = Depends(uow_dependency)):
+    usecase = RemoveBookFromLibraryUseCase(uow)
     try:
         remove_book = await usecase(user_id,book_id)
         return remove_book
