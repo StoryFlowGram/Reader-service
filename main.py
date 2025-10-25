53c6256ac5b97ae067f567486d289efe6a3026cf
Squilix01 <Squilix01@no-reply.com>
2025-10-26 00:51:45 +0300

    feat: remove flush_database. add dependency overrides for uow

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from app.presentation.api.v1.user_book.user_book_controller import user_book_rou
 from app.presentation.api.v1.reading_progress.reading_progress_controller import reading_progress_router
 from app.infrastructure import di
 from app.presentation.api import depends
-from app.infrastructure.database.engine import flush_database, engine
 
 
 
@@ -18,8 +17,4 @@ app.include_router(reading_progress_router)
 
 app.dependency_overrides[depends.book_protocol] = di.book_protocol
 app.dependency_overrides[depends.reading_progress_protocol] = di.reading_progress_protocol
-
-@app.on_event("startup")
-async def startup_event():
-    await flush_database(engine)
-    pass
+app.dependency_overrides[depends.uow_dependency] = di.uow_dependency
