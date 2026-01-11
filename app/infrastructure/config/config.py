795647731811869bc188567757e9ac3358bf24ea
Squilix <Squilix@example.com>
2026-01-11 14:20:12 +0300

    refactor(infra): change config

--- app/infrastructure/config/config.py
+++ app/infrastructure/config/config.py
@@ -3,18 +3,18 @@ from sqlalchemy import URL
 
 
 class DatabaseConfig(BaseSettings):
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
     DB_NAME: str
 
 
     def sqlalchemy_database_url(self, DB_API: str) -> URL:
         return URL.create(
             drivername=f"postgresql+{DB_API}",
-            username=self.DB_USER,
-            password=self.DB_PASSWORD,
-            host=self.DB_HOST,
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_DB,
             database=self.DB_NAME,
         )
     
@@ -23,7 +23,15 @@ class DatabaseConfig(BaseSettings):
         "env_file_encoding": "utf-8"
     }
 
+class URLConfig(BaseSettings):
+    BOOK_SERVICE_URL: str
+
+    model_config = {
+        "extra": "ignore",
+        "env_file_encoding": "utf-8"
+    }
 
 class Config:
     def __init__(self, env_file: str | None = None):
         self.db = DatabaseConfig(_env_file=env_file)
+        self.url = URLConfig(_env_file=env_file)
